0d30f2cedf53c853eb97588749832797a8b44f12
morph <emmanuel.stephant@laposte.net>
2023-12-12 22:00:27 +0100

    Ajout du mode de connexion, de l'utilisateur "Test" "test", Ajout du comportement de connexion Ajout d'un fichier explicatif du projet, README.md

---
+++ SimpleTutorials.Wpf.Authentication/ViewModels/LoginViewViewModel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using SimpleTutorials.Core;
+using SimpleTutorials.DbLib.Model;
+using SimpleTutorials.Wpf.Authentication.Services.Authentication;
+
+namespace SimpleTutorials.Wpf.Authentication.ViewModels
+{
+    /// <summary>
+    /// VM de <see cref="SimpleTutorials.Wpf.Authentication.Views.LoginView"/>
+    /// </summary>
+    internal class LoginViewViewModel : ObservableObject
+    {
+        #region Fields
+
+        /// <summary>
+        /// Utilisateur
+        /// </summary>
+        private string? _UserName;
+
+        /// <summary>
+        /// Mot de passe
+        /// </summary>
+        private string? _Password;
+
+        /// <summary>
+        /// Message
+        /// </summary>
+        private string? _Message;
+
+        /// <summary>
+        /// Indique si l'utilisateur est connecté ou non.
+        /// </summary>
+        private bool? _IsLoggedIn;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Obtient ou défini <see cref="_UserName"/>
+        /// </summary>
+        public string? UserName { get => _UserName; set => _UserName = value; }
+
+        /// <summary>
+        /// Obtient ou défini <see cref="_Password"/>
+        /// </summary>
+        public string? Password { get => _Password; set => _Password = value; }
+
+        /// <summary>
+        /// Obtient ou défini <see cref="_Message"/>
+        /// Vu qu'on veut actualisé la vue lors du changement, on utilise <see cref="ObservableObject.SetProperty{T}(string, ref T, T, bool)"/>
+        /// </summary>
+        public string? Message { get => _Message; set => SetProperty(nameof(Message), ref _Message, value); }
+        
+        /// <summary>
+        /// Obtient ou défini <see cref="_IsLoggedIn"/>
+        /// </summary>
+        public bool? IsLoggedIn { get => _IsLoggedIn; private set => SetProperty(nameof(IsLoggedIn), ref _IsLoggedIn, value); }
+
+        #endregion
+
+
+        #region Constructors
+
+        /// <summary>
+        ///  Constructeur
+        /// </summary>
+        public LoginViewViewModel()
+        {
+            IsLoggedIn = false;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Méthode de d'authentification d'un utilisateur
+        /// </summary>
+        internal void Authenticate()
+        {
+            // Outil de hashage
+            PasswordHasher hasher = new PasswordHasher();
+
+            PasswordVerificationResult result = PasswordVerificationResult.Failed;
+
+            // L'utilisateur récupéré
+            User? user = null;
+            // On recherche l'utilisateur par son identifiant
+            using (SimpleTutorialsDBContext context = new())
+                user = context.Users.FirstOrDefault(userTemp => userTemp.UserName.Equals(UserName));
+
+            // Si il n'existe pas, on renvoie une erreur
+            if (user == null)
+                Message = "Impossible de trouver l'utilisateur";
+            else
+                result = hasher.VerifyHashedPassword(user.HashedPassword, Password);
+
+            switch (result)
+            {
+                case PasswordVerificationResult.Failed:
+                    Message = "Mot de passe incorrect";
+                    break;
+                case PasswordVerificationResult.Success:
+                    // On défini le logging à true. La vue observe cette propriété et va se cacher si IsLogging = true.
+                    IsLoggedIn = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ SimpleTutorials.Wpf.Authentication/Views/LoginView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SimpleTutorials.Wpf.Authentication.ViewModels;
+
+namespace SimpleTutorials.Wpf.Authentication.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructeur
+        /// Initialise le dataContext
+        /// </summary>
+        public LoginView()
+        {
+            InitializeComponent();
+            this.DataContext = new LoginViewViewModel();
+        }
+        #endregion
+
+        #region Methods
+
+
+        /// <summary>
+        /// Met à jour le mot de passe du VM associé
+        /// </summary>
+        /// <param name="sender">le contrôle qui envoie l'event, ici une passwordbox</param>
+        /// <param name="e">les éventuels arguments</param>
+        private void PasswordBoxLogin_PasswordChanged(object sender, RoutedEventArgs e)
+            => ((LoginViewViewModel)this.DataContext).Password = ((PasswordBox)sender).Password;
+
+        /// <summary>
+        /// Gère la tentative de login
+        /// </summary>
+        /// <param name="sender">Bouton qui envoie la demande</param>
+        /// <param name="e">Les éventuels arguments</param>
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+            => ((LoginViewViewModel)this.DataContext).Authenticate();
+
+        #endregion
+
+
+    }
+}
