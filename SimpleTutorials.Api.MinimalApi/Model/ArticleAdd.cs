776f41352e9d87041f0fddef7cc804a2eaa75995
morph <emmanuel.stephant@laposte.net>
2024-01-02 12:20:52 +0100

    Exemple de minimal Api Basé sur la vidéo de Christophe Mommer https://www.youtube.com/watch?v=CXQrai-PF5M&t=4511s

---
+++ SimpleTutorials.Api.MinimalApi/Model/ArticleAdd.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace SimpleTutorials.Api.MinimalApi.Model
+{
+    /// <summary>
+    /// Enregistrement d'un article
+    /// </summary>
+    public record ArticleAdd
+    {
+
+        /// <summary>
+        /// Nom
+        /// </summary>
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// Constructeur par défaut pour le proxy d'ajout
+        /// </summary>
+        /// <param name="identifier">Identifiant</param>
+        /// <param name="name">Nom</param>
+        public ArticleAdd(string name)
+        {
+            Name = name;
+
+        }
+
+
+        public static async ValueTask<ArticleAdd> BindAsync(HttpContext context, ParameterInfo parameterInfo)
+        {
+            using StreamReader? stream = new StreamReader(context.Request.Body);
+            Task<string>? body = stream.ReadToEndAsync();
+
+            ArticleAdd proxy = new(body.Result);
+
+
+
+            return proxy;
+        }
+    }
+}
---
+++ SimpleTutorials.Api.MinimalApi/Service/ArticleService.cs
@@ -0,0 +1,81 @@
+﻿
+using SimpleTutorials.Api.MinimalApi.Model;
+
+namespace SimpleTutorials.Api.MinimalApi.Service
+{
+    /// <summary>
+    /// Services pour les <see cref="Article"/>
+    /// </summary>
+    public class ArticleService
+    {
+        #region Properties
+
+        /// <summary>
+        /// Liste des articles
+        /// </summary>
+        private List<Article> Articles { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructeur, instancie les deux premiers articles de la liste.
+        /// </summary>
+        public ArticleService()
+        {
+
+            Articles = new List<Article>()
+            {
+                new Article(1, "Article 1"),
+                new Article(2, "Article 2"),
+            };
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Ajoute un nouvel article à la liste
+        /// </summary>
+        /// <param name="name">Nom de l'article à ajouter</param>
+        /// <returns></returns>
+        public Article AddArticle(string name)
+        {
+            Article article = new Article(Articles.Max(id => id.Identifier) + 1, name);
+            Articles.Add(article);
+            return article;
+        }
+
+        /// <summary>
+        /// Retourne la liste des articles
+        /// </summary>
+        /// <returns></returns>
+        public List<Article> GetArticles() => Articles;
+
+        /// <summary>
+        /// Retourne l'article de l'identifiant correspondant
+        /// </summary>
+        /// <param name="identifier">Identifiant</param>
+        /// <returns></returns>
+        public Article? GetArticleById(int identifier) => Articles.Find(a => a.Identifier == identifier);
+
+        /// <summary>
+        /// Obtient l'article par son nom
+        /// </summary>
+        /// <param name="name">Nom</param>
+        /// <returns></returns>
+        public Article? GetArticleByName(string name) => Articles.Find(a => a.Name == name);
+
+        /// <summary>
+        /// Supprime l'article par son identifiant
+        /// </summary>
+        /// <param name="identifier"></param>
+        public void DeleteArticle(int identifier) =>
+            Articles.Remove(GetArticleById(identifier) ?? throw new Exception("Aucun article associé"));
+
+        #endregion
+
+    }
+}
