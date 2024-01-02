7ea24f216b0cc446966aa89ab7a730824944ff9c
morph <emmanuel.stephant@laposte.net>
2024-01-02 10:38:13 +0100

    Initialisation du projet

---
+++ SimpleTutorials.DPIWpf.Wpf/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SimpleTutorials.DPIWpf.Wpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    
+    /// <summary>
+    /// Hôte nécessaire à l'injection de dépendance
+    /// </summary>
+    public static IHost? AppHost { get; private set; }
+
+
+    /// <summary>
+    /// Constructeur par défaut. Initialise les services
+    /// </summary>
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) => 
+            {
+                services.AddSingleton<MainWindow>();
+            
+            }).Build() ;
+
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        // Démarrage de l'application
+        await AppHost!.StartAsync();
+
+        MainWindow? startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+        startupForm.Show();
+
+
+        base.OnStartup(e);
+    }
+
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit(e);
+    }
+}
---
+++ SimpleTutorials.DPIWpf.Wpf/README.md
@@ -0,0 +1,8 @@
+# DPI en WPF
+
+# Introduction
+
+Basé sur la vidéo suivante https://www.youtube.com/watch?v=dLR_D2IJE1M
+
+
+# App.xaml.cs
